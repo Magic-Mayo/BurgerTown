a309f0d296aac93fa1980e6d2453c27d07977156
MagicMayo <mike.mayoii@gmail.com>
2019-08-23 15:08:57 -0700

    made a post route for new burgers added and started working on a put route

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -7,9 +7,23 @@ router.get('/', (req,res)=>{
         const burgers = {
             burgers: data
         }
-        console.log(burgers);
+        // console.log(burgers);
         res.render('index', burgers);
     });
+});
+
+router.post('/burgers/new', (req,res)=>{
+    burger.create('burger_name', req.body.newburger, (results)=>{
+        res.redirect('/')
+    })
+});
+
+router.put('/burgers/devour', (req,res)=>{
+    console.log(req.body)
+    // burger.update({devoured: req.body, req.body.id, (results)=>{
+    //     console.log(results)
+    //     res.redirect('/')
+    // });
 })
 
 module.exports = router;
